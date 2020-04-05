815f5d2fa1637a63a592812acbddbcd88b52dea5
ataoglan <ozcanataoglan@gmail.com>
2020-04-05 14:02:36 +0300

    first commit

---
+++ src/main/java/com/ataoglan/websocketdemo/SocketTextHandler.java
@@ -0,0 +1,20 @@
+package com.ataoglan.websocketdemo;
+
+
+import org.json.JSONObject;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+
+public class SocketTextHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession webSocketSession, TextMessage message) throws Exception {
+            String payload = message.getPayload();
+            JSONObject jsonObject = new JSONObject(payload);
+            webSocketSession.sendMessage(new TextMessage("Merhaba" + jsonObject.get("user") + "bugün sana nasıl yardımcı olabilirim?"));
+
+    }
+}
